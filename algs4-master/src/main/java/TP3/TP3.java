0af387e178d2135856eda21558df30de3b389c58
SebastienCadorette <cado.94@me.com>
2018-10-28 16:25:31 -0400

    Test rapporteru pour l'attribut first.

--- algs4-master/src/main/java/TP3/TP3.java
+++ algs4-master/src/main/java/TP3/TP3.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import edu.princeton.cs.algs4.Queue;
+import java.util.NoSuchElementException;
 
 class TP3 {
 
@@ -27,4 +28,26 @@ class TP3 {
 		assertEquals(q.size(), 0);
 	}
 	
+	/**
+	 * d2 = < { Queue() -> peek() } , { peek() == Error Queue underflow } >
+	 */
+	@Test
+	void rapporteur_first_vide() {
+		Queue q = new Queue();
+		assertThrows(NoSuchElementException.class, () -> {
+			q.peek();
+		}, "Devrait retourner une erreur indiquant qu'il n'y a pas assez d'element pour faire un peek.");
+	}
+	
+	/**
+	 * d3 = < { Queue() -> enqueue(1) -> peek() } , { peek() == 1 } >
+	 */
+	@Test
+	void rapporteur_first_nonVide() {
+		Queue<Integer> q = new Queue<Integer>();
+		int val = 1;
+		q.enqueue(val);
+		int result = q.peek();
+		assertEquals(result, val);
+	}
 }
